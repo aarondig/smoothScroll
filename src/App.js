9be2c6c78ee8e2b499c21ec935649493a03975a8
Aaron Diggdon <aarondiggdon@gmail.com>
2021-01-04 17:21:15 -0800

    Added loop to scroll and completed with css skew

--- src/App.js
+++ src/App.js
@@ -9,13 +9,43 @@ function App() {
   const app = useRef();
   const scrollContainer = useRef();
 
+  const data = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
   useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
 
-    console.log(size.height)
+  useEffect(() => {
     document.body.style.height = `${
       scrollContainer.current.getBoundingClientRect().height
     }px`;
   }, [size.height]);
+
+
+// SCROLLING
+  const skewScrolling = () => {
+    //Set Current to the scroll position amount
+    data.current = window.scrollY;
+    // Set Previous to the scroll previous position
+    data.previous += (data.current - data.previous) * data.ease;
+      // Set rounded to
+    data.rounded = Math.round(data.previous * 100) / 100;
+
+    //VARIABLES
+    const difference = data.current - data.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.5;
+
+    //Assign skew and smooth scrolling to the scroll container
+    scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0) skewY(${skew}deg)`;
+
+    requestAnimationFrame(() => skewScrolling());
+  };
 
   return (
     <div ref={app} className="App">
