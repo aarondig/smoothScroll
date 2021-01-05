3d32f150dd7c627f2186431d1078631737cbef8d
Aaron Diggdon <aarondiggdon@gmail.com>
2021-01-04 16:40:10 -0800

    Added images and base css stylings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react"
+import images from "./components/content"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="scroll">hello world</div>
+      <div className="scroll">
+        {images.map((image, index) => (
+          <div key={index} className="img-container">
+            <img src={image} />
+            </div>
+            
+        ))}
+      </div>
     </div>
   );
 }
