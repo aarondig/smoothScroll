4ca1bb04852ec37242d52c7f7227edbae683b2ae
Aaron Diggdon <aarondiggdon@gmail.com>
2021-01-04 17:00:30 -0800

    Created resizing hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from "react"
-import images from "./components/content"
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import images from "./components/content";
+import useWindowSize from "./hooks/windowSize";
+import "./App.css";
 
 function App() {
+  const size = useWindowSize();
+
+  const app = useRef();
+  const scrollContainer = useRef();
+
+  useEffect(() => {
+
+    console.log(size.height)
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`;
+  }, [size.height]);
+
   return (
-    <div className="App">
-      <div className="scroll">
+    <div ref={app} className="App">
+      <div ref={scrollContainer} className="scroll">
         {images.map((image, index) => (
           <div key={index} className="img-container">
             <img src={image} />
-            </div>
-            
+          </div>
         ))}
       </div>
     </div>
